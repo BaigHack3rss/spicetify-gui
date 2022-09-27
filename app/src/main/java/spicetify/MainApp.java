119886fc138a8915163e2353ee84249ab9f0abea
BaigHack3rss <barlasmomin586@gmail.com>
2022-09-26 22:35:29 -0400

    Added functionality to check if the user is using one of the supported operating systems. Created function that displays a window for error messages.

--- app/src/main/java/spicetify/MainApp.java
+++ app/src/main/java/spicetify/MainApp.java
@@ -4,19 +4,50 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MainApp extends Application{
+    
+    private static void error(String message){
+        Text errMessage = new Text(message);
+        Parent root = new StackPane();
+        Scene scene = new Scene(root, 600, 408, Color.rgb(28, 28, 28));
+        Stage stage = new Stage();
+        Image error = new Image("file:src/main/resources/spicetify/images/Error.png");
+        ImageView errorView = new ImageView(error);
+        
+        errorView.setTranslateY(-25);
 
-    private Parent createContent() {
-        return new StackPane(new Text("Hello World"));
-    }
+        errMessage.setFont(Font.font("source code pro", FontWeight.BOLD, 20));
+        errMessage.setFill(Color.RED);
+        errMessage.setTranslateY(175);
+
+        ((StackPane) root).getChildren().addAll(errMessage, errorView);
+
+        stage.setScene(scene);
+        stage.setTitle("Error: " + message);
+        
+        stage.show();
+        
+    } 
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent(), 300, 300));
-        stage.show();
+        OperatingSystem os = new OperatingSystem();
+        os.setOperatingSystem(System.getProperty("os.name"));
+        os.setConfigDirectory(os.getOperatingSystem());
+        if (os.getOperatingSystem().equals("Unknown")) {
+            error("Unsupported Operating System");
+        }
+        else {
+            return;
+        }
     }
 
     public static void main(String[] args) {

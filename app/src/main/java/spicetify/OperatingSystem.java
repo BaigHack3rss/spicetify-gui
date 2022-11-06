5d53dbffc1000e13eef927c8dc99de9ade9a9136
BaigHack3rss <barlasmomin586@gmail.com>
2022-11-05 23:33:11 -0400

    Added methods and refactored Added some methods to OperatingSystem.java and removed unneeded methods from OperatingSystem.java

--- app/src/main/java/spicetify/OperatingSystem.java
+++ app/src/main/java/spicetify/OperatingSystem.java
@@ -1,63 +1,140 @@
 package spicetify;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javafx.scene.image.Image;
 
 public class OperatingSystem {
-    private String OperatingSystem;
-    private String configDirectory;
+    private String operatingSystem, slashType, configDirectory;
     private String[] themes;
 
-    public OperatingSystem() {
+    public OperatingSystem(String OS) {
+        if (OS.toLowerCase().contains("win")){
+            operatingSystem = "Windows";
+            slashType = "\\";
+            configDirectory = System.getenv("APPDATA") + slashType + "Spotify";
+        }
+        else if (OS.toLowerCase().contains("mac")){
+            operatingSystem = "MacOS";
+            slashType = "/";
+            configDirectory = System.getProperty("user.home") + slashType + ".config" + slashType + "spicetify";
+
+        }
+        else if (OS.toLowerCase().contains("nix") || OS.toLowerCase().contains("nux") || OS.toLowerCase().contains("aix")){
+            operatingSystem = "Linux";
+            slashType = "/";
+            configDirectory = System.getProperty("user.home") + slashType + ".config" + slashType + "spicetify";
+        }
+        else {
+            operatingSystem = "Unknown";
+        }
     }
 
-    public OperatingSystem(String operatingSystem, String configDirectory) {
-        OperatingSystem = operatingSystem;
-        this.configDirectory = configDirectory;
+    public boolean readyToGo() {
+        try{
+            File cD = new File(configDirectory);
+            File cF = new File(configDirectory + slashType + "config-xpui.ini");
+            ProcessBuilder pb = new ProcessBuilder("spicetify", "-v");
+            Process p = pb.start();
+            Scanner s = new Scanner(p.getInputStream());
+            String spicetifyVersion = s.nextLine();
+            s.close();
+            String[] spicetifyVersionArray = spicetifyVersion.split(".");
+            boolean validVersion = true;
+            // if parsing the spicetify version array as int is unsuccessful, set validVersion to false
+            for (int i = 0; i < spicetifyVersionArray.length; i++) {
+                try {
+                    Integer.parseInt(spicetifyVersionArray[i]);
+                }
+                catch (NumberFormatException e) {
+                    validVersion = false;
+                }
+            }
+            if (cD.exists() && cF.exists() && validVersion) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String readConfig(String field) {
+        try{
+            Scanner configScanner = new Scanner(new File(configDirectory + slashType + "config-xpui.ini"));
+            String value = "";
+            while (configScanner.hasNextLine()) {
+                String line = configScanner.nextLine();
+                if (line.contains(field)) {
+                    value = line.substring(line.indexOf("=") + 1);
+                }
+            }
+
+            value = value.replaceAll("\\s", "");
+            return value;
+
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+
+    public Image getThemePreview(String themeName, String variant) throws IOException {
+        String themePath = configDirectory + slashType + "Themes" + slashType + themeName;
+        Image image = new Image(getClass().getResourceAsStream("images/essentials/icon.png"));
+        File themeFolder = new File(themePath);
+        // if theme folder doesn't exist, change themeName to lowercase and try again
+        if (!themeFolder.exists()) {
+            themeName = themeName.toLowerCase();
+            themePath = configDirectory + slashType + "Themes" + slashType + themeName;
+            themeFolder = new File(themePath);
+            // if it still doesn't exist, return the default preview image
+            if (!themeFolder.exists()) {
+                image = new Image(getClass().getResourceAsStream("images/essentials/Preview.png"));
+            }
+        } else {
+            // check if there is a screenshots folder
+            File screenshotsFolder = new File(themePath + slashType + "screenshots");
+            if (screenshotsFolder.exists()) {
+                // check set image to the image that matches the variant
+                File[] screenshots = screenshotsFolder.listFiles();
+                for (File screenshot : screenshots) {
+                    if (screenshot.getName().contains(variant) || screenshot.getName().contains(variant.toLowerCase())) {
+                        image = new Image(screenshot.toURI().toString());
+                    }
+                }
+            }
+            else {
+                // if there is no screenshots folder, check if there is a preview image
+                File[] themeFiles = themeFolder.listFiles();
+                for (File themeFile : themeFiles) {
+                    if (themeFile.getName().contains(variant) || themeFile.getName().contains(variant.toLowerCase())) {
+                        image = new Image(themeFile.toURI().toString());
+                    }
+                }
+            }
+        }
+        return image;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public String getSlashType() {
+        return slashType;
+    }
+
+    public String getConfigDirectory() {
+        return configDirectory;
     }
 
     public String[] getThemes() {
         return themes;
-    }
-
-    public void setThemes(String[] themes) {
-        this.themes = themes;
-    }
-
-    public void setOperatingSystem(String OS){
-        if (OS.toLowerCase().contains("win")){
-            this.OperatingSystem = "Windows";
-        }
-        else if (OS.toLowerCase().contains("mac")){
-            this.OperatingSystem = "MacOS";
-        }
-        else if (OS.toLowerCase().contains("nix") || OS.toLowerCase().contains("nux") || OS.toLowerCase().contains("aix")){
-            this.OperatingSystem = "Linux";
-        }
-        else {
-            this.OperatingSystem = "Unknown";
-        }
-    } 
-
-    public String getOperatingSystem(){
-        return this.OperatingSystem;
-    }
-
-    public void setConfigDirectory(String OS){
-        if (OS.equals("Windows")){
-            this.configDirectory = System.getenv("APPDATA") + "\\spicetify";
-        }
-        else if (OS.equals("Linux") || OS.equals("MacOS")){
-            this.configDirectory = System.getProperty("user.home") + "/.config/spicetify";
-        }
-    }
-
-    public String getConfigDirectory(){
-        return this.configDirectory;
-    }
-
-    public String[] listDirectory(String directory) {
-        File file = new File(directory);
-        String[] files = file.list();
-        return files;
     }
 }
